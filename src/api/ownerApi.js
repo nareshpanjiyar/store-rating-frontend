7446fdd7d1d5d35ed15d6b4288509387b8f4ad90
Naresh Panjiyar <nareshpanjiyar90@gmail.com>
2026-06-06 22:25:03 +0530

    Undo storeId check

--- src/api/ownerApi.js
+++ src/api/ownerApi.js
@@ -1,10 +1,8 @@
 import api from "./axios";
 
 export const getOwnerDashboard = async (storeId) => {
-  if (storeId) {
-    const response = await api.get(`/api/owner/dashboard?storeId=${storeId}`);
-    return response.data;
-  }
+  const response = await api.get(`/api/owner/dashboard?storeId=${storeId}`);
+  return response.data;
 };
 
 export const changePassword = async (payload) => {
